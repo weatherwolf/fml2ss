7b0361d02e248c6ae3990986bca26649c88e678e
Tim Knip <tim@floorplanner.com>
2025-08-20 17:10:46 +0200

    adding python subprocess example

--- fpe/src/execute_process_design.py
+++ fpe/src/execute_process_design.py
@@ -1,5 +1,14 @@
 import os
 import subprocess
+import json
+import re
+
+def get_design_from_output(output):
+    for r in re.split('[\r\n]', output):
+        try:
+            return json.loads(r)
+        except:
+           pass
 
 if __name__ == '__main__':
     p = subprocess.Popen([
@@ -10,10 +19,12 @@ if __name__ == '__main__':
         '--design-id',
         '115981800',
         '--outfile',
-        'timtest/design.fml'
+        'tim/design.fml'
     ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output, errors = p.communicate()
     print('output')
-    print(output)
+    enhanced_design_fml = get_design_from_output(output)
+    print('#areas: %d' % len(enhanced_design_fml['areas']))
+    print('#walls: %d' % len(enhanced_design_fml['walls']))
     print('errors')
     print(errors)
--- fpe/src/process_design.mjs
+++ fpe/src/process_design.mjs
@@ -27,7 +27,7 @@ async function execute ({projectId, designId, outfile}) {
         if (outfile) {
             fs.writeFileSync(outfile, JSON.stringify(result, null, 2));
         }
-        console.log(JSON.stringify(result, null, 2))
+        console.log(JSON.stringify(result))
     } else {
         console.error(`project ${projectId} does not have a design wiht id ${designId}`);
         process.exit(1);
